6e47e964f59b1e3b7be86423bd533423aef7c45d
Adam Heath <doogie@brainfood.com>
2023-06-12 09:46:20 -0500

    Update submodule with improved multi-agent vhost support.

--- apiserver.sh
+++ apiserver.sh
@@ -5,7 +5,7 @@ set -e
 TOP_DIR="$(cd "$(dirname "$0")/."; echo "$PWD")"
 export TOP_DIR
 
-"$TOP_DIR/docker-apiserver/scripts/apiserver.sh" --context-dir "$TOP_DIR" -f "$TOP_DIR/registry-compose.yaml" -n registry --agent-count 1 "$@"
+"$TOP_DIR/docker-apiserver/scripts/apiserver.sh" --context-dir "$TOP_DIR" -f "$TOP_DIR/registry-compose.yaml" -n registry --agent-count 2 "$@"
 #"$TOP_DIR/docker-apiserver/scripts/apiserver.sh" --context-dir "$TOP_DIR" -f "$TOP_DIR/render-node-compose.yaml" -n blender-render-1 -n registry --agent-count 1 "$@"
 case "$1" in
 	(start)
